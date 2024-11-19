315ba6e6715c2778fe812504ea099f2ae1ea457a
Aparna <aparna123jul@gmail.com>
2024-11-19 09:01:33 -0500

    Initial Commit

---
+++ Avatar/AvatarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvatarApp.swift
+//  Avatar
+//
+//  Created by Aparna Nimmagadda on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct AvatarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AvatarTests/AvatarTests.swift
@@ -0,0 +1,17 @@
+//
+//  AvatarTests.swift
+//  AvatarTests
+//
+//  Created by Aparna Nimmagadda on 11/19/24.
+//
+
+import Testing
+@testable import Avatar
+
+struct AvatarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
