bf2611f96d93e1eb07940752115313a0f412c32b
Aparna <aparna123jul@gmail.com>
2024-12-03 02:21:13 -0500

    initial commit

--- Avatar/ContentView.swift
+++ Avatar/ContentView.swift
@@ -1,24 +1,47 @@
-//
-//  ContentView.swift
-//  Avatar
-//
-//  Created by Aparna Nimmagadda on 11/19/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 40) {
+            // Avatar VStack
+            VStack {
+                Image("avatar") // Reference to the image added to Assets
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200) // Adjust size as needed
+                    .clipShape(Circle()) // Make it circular
+                    .shadow(radius: 20) // Add a shadow for aesthetics
+                Text("Avatar") // Label for the avatar
+                    .font(.caption)
+            }
+            
+            // Name VStack
+            VStack {
+                Text("Aparna Sai Nimmagadda") // User's name
+                    .multilineTextAlignment(.center)
+                    .font(.system(size: 36,weight: .bold))
+                    .fontWeight(.bold)
+            }
+            
+            // MySpace Link VStack
+            VStack {
+                Link(destination: URL(string: "https://github.com/AparnaSai1")!) {
+                    Text("Visit MySpace Profile")
+                        .font(.title2)
+                        .foregroundColor(.blue)
+                        .underline()
+                }
+                    .font(.caption)
+            }
         }
-        .padding()
+        .padding(.horizontal,50) // Adds padding around the entire VStack
+        .padding(.vertical,60)
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+
